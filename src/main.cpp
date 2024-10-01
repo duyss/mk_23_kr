6680b7ef3fd012dcbcf001929f7edcad051bf5cb
duyss <50496059+duyss@users.noreply.github.com>
2024-10-01 14:05:25 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,57 @@
+#include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/spi.h>
 
-void setup_usb () {
-    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO11 | GPIO12);
-	gpio_set_af(GPIOA, GPIO_AF10, GPIO11 | GPIO12);
-    }
+static void spi_setup(void)
+{
+    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO5 | GPIO6 | GPIO7);
+    gpio_set_af(GPIOA, GPIO_AF5, GPIO5 | GPIO6 | GPIO7);
+    gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_2MHZ, GPIO5 | GPIO6 | GPIO7);
 
-void setup_spi () {
-    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO4 | GPIO5 | GPIO6 | GPIO7 ); //CS, SCK, MISO, MOSI                
-    gpio_set_af(GPIOA, GPIO_AF5, GPIO4 | GPIO5 | GPIO6 | GPIO7);                                                                                           
-    }
+    spi_disable(SPI1);
+    spi_set_master_mode(SPI1);
+    spi_set_baudrate_prescaler(SPI1, SPI_CR1_BR_FPCLK_DIV_8);
+    spi_set_clock_polarity_0(SPI1);
+    spi_set_clock_phase_0(SPI1);
+    spi_send_msb_first(SPI1);
+    spi_enable_software_slave_management(SPI1);
+    spi_set_nss_high(SPI1);
 
-void setup_jtag () {
-    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO13 | GPIO14 | GPIO15); // TMS, TCK, TDI
-    gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO3 | GPIO4); // TDO, RST
-    gpio_set_af(GPIOA, GPIO_AF1, GPIO13 | GPIO14 | GPIO15);
-    gpio_set_af(GPIOB, GPIO_AF2, GPIO3 | GPIO4);
+    spi_enable(SPI1);
 }
 
-int main () {}
+static void clock_setup(void)
+{
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_GPIOD);
+    rcc_periph_clock_enable(RCC_SPI1);
+}
+
+static void gpio_setup(void)
+{
+	gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO8);
+	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT,
+			GPIO_PUPD_NONE, GPIO12 | GPIO13 | GPIO14 | GPIO15);
+}
+
+
+int main(void)
+{
+    int i;
+
+	clock_setup();
+	gpio_setup();
+    spi_setup();
+
+	while(1) {
+		if(gpio_get(GPIOA, GPIO8)){
+            for (i = 0; i < 30000; i++) {
+			    gpio_toggle(GPIOD, GPIO12 | GPIO13 | GPIO14 | GPIO15);
+                spi_write(SPI1, 0x31);
+            }
+		}
+
+	}
+
+	return 0;
+}
